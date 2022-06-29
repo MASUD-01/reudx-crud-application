13945456511c069fdc1619f0d2475584ce5caac9
MASUD-01 <masudrana96141@gmail.com>
2022-06-29 18:46:36 +0600

    add navbar

---
+++ src/Layouts/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/show-books">Show books</Link>
+            <Link to="/add-books">Add books</Link>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/routes/Index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from '../Layouts/Navbar';
+import Error from '../pages/Error';
+import Home from '../pages/Home';
+
+const Index = () => {
+    return (
+        <BrowserRouter>
+            <Navbar></Navbar>
+            <Routes>
+                <Route path='/' element={<Home></Home>}>
+                </Route>
+                <Route path='*' element={<Error></Error>}>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Index;
